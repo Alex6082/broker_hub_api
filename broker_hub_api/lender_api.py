35497e95100619ed22c3588338c9e14b6f5b5fc4
bohda <bohdangavryschuk@gmail.com>
2021-08-31 13:01:28 +0300

    fix lender api and added description for method

--- broker_hub_api/lender_api.py
+++ broker_hub_api/lender_api.py
@@ -2,23 +2,32 @@ from broker_hub_api.base_api import CommonBrokerHubApi
 
 
 class LenderApi(CommonBrokerHubApi):
-    def send_decision(self, uid, decision: str, redirect_url):
+    def send_decision(self, uid, decision: str):
+        """
+        Obtaining a decision to issue a loan from lender, after lender send request
+        """
+
         decision = decision.lower()
         if decision not in ["approved", "rejected"]:
             raise ValueError('Decision may be be either "approved" or "rejected"')
 
         data = dict(
             uid=uid,
-            decision=decision,
-            redirect_url=redirect_url
+            decision=decision
         )
         return self.post('/lender/set_loan_request_decision/', data=data)
 
-    def send_ready_to_issue(self, uid, signed, guaranteed, guaranty_data):
+    def send_ready_to_issue(self, uid, processed_result):
+        """
+        Receiving a response on the result of loan processing
+        """
+
+        processed_result = processed_result.lower()
+        if processed_result not in ["signed", "client_refusal"]:
+            raise ValueError('Processed result may be be either "signed" or "client_refusal"')
+
         data = dict(
             uid=uid,
-            signed=signed,
-            guaranteed=guaranteed,
-            guaranty_data=guaranty_data,
+            processed_result=processed_result
         )
         return self.post('/lender/loan_processed_callback/', data=data)
